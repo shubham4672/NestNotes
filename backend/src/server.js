a4b893b7dfd97bcff838bb76c0dacd6fb750985a
shubham4672 <shubham4672.20@gmail.com>
2025-09-22 11:03:04 +0530

    production

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 
+const __dirname = path.resolve();
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE", "PUT"]
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: ["http://localhost:5173", "http://localhost:3000"],
+      methods: ["GET", "POST", "DELETE", "PUT"],
+    })
+  );
+}
 
 const limiter = rateLimit({
   windowMs: 20 * 1000,
@@ -28,6 +32,12 @@ const limiter = rateLimit({
 app.use(express.json());
 app.use(limiter);
 app.use("/api/notes", notesRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "production" ? "/api" : "http://localhost:3000/api";
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
